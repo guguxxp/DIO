04bbb34df8eb4ca61f3aa5dc98adfd44f229df38
guguxxp <gustavo.strauss53@gmail.com>
2021-12-05 23:12:01 -0300

    Subindo exercicio2 da aula de métodos Java

---
+++ Bootcamp Cognizant - Java/IntelliJ - Workspace/src/com/dio/metodos/sobrecarga/MainExercicio2.java
@@ -0,0 +1,12 @@
+package com.dio.metodos.sobrecarga;
+
+public class MainExercicio2 {
+    public static void main(String[] args) {
+
+        System.out.println("Valores Exercicio Quadrilatero");
+        Quadrilatero.area(4);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(6f, 6f);
+    }
+}
